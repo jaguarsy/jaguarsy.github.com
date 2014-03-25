75d2c010ec2631c74528385cd54f39b4e9eb8b79
JohnnyCage <395703303@qq.com>
2014-03-25 22:29:34 +0800

    增加文章获取显示功能。

--- js/fun.js
+++ js/fun.js
@@ -1,7 +1,32 @@
 var root = 'https://395703303blog.firebaseIO-demo.com/';
+var articleRef;
+var articles;
+
+//模板解析
+String.prototype.temp = function(obj) {
+    return this.replace(/\$\w+\$/gi, function(matchs) {
+        var returns = obj[matchs.replace(/\$/g, "")];
+        return (returns + "") == "undefined" ? "" : returns;
+    });
+}
+
+String.prototype.getDom = function() {
+　　 var objE = document.createElement("div");
+　　 objE.innerHTML = this;
+　　 return objE.childNodes;
+};
 
 $(function(){
-    
-    
-    
+    var template = $("#template-article").html();
+    var home = $("#home");
+
+    articleRef = new Firebase(root + 'articles/');
+
+    articleRef.limit(50).on('child_added', function (snapshot) {
+    	var art = snapshot.val();
+    	home.append(template.temp(art));
+    	var child = $('#home>div:last-child').hide();
+    	child.fadeIn();
+  	});
+
 });
