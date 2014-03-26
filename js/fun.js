0ccc30e274658b05e7d1d3925399e1ad73bec9f0
Johnny <jaguarsy14@gmail.com>
2014-03-26 13:18:12 +0800

    修改文章显示界面。增加文章创建功能。

--- js/fun.js
+++ js/fun.js
@@ -10,21 +10,28 @@ String.prototype.temp = function(obj) {
     });
 }
 
-String.prototype.getDom = function() {
-　　 var objE = document.createElement("div");
-　　 objE.innerHTML = this;
-　　 return objE.childNodes;
-};
-
 $(function(){
     var template = $("#template-article").html();
     var home = $("#home");
 
     articleRef = new Firebase(root + 'articles/');
-
+    
+    //for test
+    //articleRef.push({id:Guid.NewGuid().ToString(),title:"测试标题", content:"测试内容", author:"Johnny", time:getTime()});
+    
     articleRef.limit(50).on('child_added', function (snapshot) {
     	var art = snapshot.val();
     	home.append(template.temp(art));
   	});
 
 });
+
+function getTime(){ 
+  var date = new Date();
+  var hour = date.getHours();
+  var min_ = date.getMinutes(); 
+  if(hour<10)hour="0"+hour;
+  if(min_<10)min_="0"+min_;
+
+  return date.toLocaleDateString()+" "+hour+"时"+min_+"分";
+}
