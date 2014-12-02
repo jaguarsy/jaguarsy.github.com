cf8f400e734ec05c77715926a9aecd37872ff379
JohnnyCage <395703303@qq.com>
2014-12-02 16:30:02 +0800

    修改为路由无刷新方式，使用firebase提供的登录注册功能。

--- FATest/js/index.js
+++ FATest/js/index.js
@@ -1,25 +1,102 @@
-var testApp = angular.module('testApp', []);
+"use strict"
 
-testApp.controller('testController', ['$scope', '$timeout', function($scope, $timeout) {
+var testApp = angular.module('testApp', ['ngRoute']);
 
-	$scope.loaded = false;
-
-	var db = new Firebase("bitcage.firebaseio.com/test/list");
-
-	$scope.list = [];
-
-	db.on("child_added", function(snapshot) {
-		$timeout(function() {
-			$scope.list.push(snapshot.val());
-			$scope.loaded = true;
-		}, 0);
-	});
-
-
-	$scope.submit = function() {
-		db.push({
-			value: $scope.inputValue
+testApp.config(['$routeProvider', function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'loginController',
+			templateUrl: 'views/login.html'
+		})
+		.when('/chat', {
+			controller: 'chatController',
+			templateUrl: 'views/chat.html'
+		})
+		.otherwise({
+			redirectTo: '/'
 		});
-	}
+}]);
 
-}])
+testApp.service('dbService', function() {
+	var dbContext = new Firebase("bitcage.firebaseio.com");
+
+	this.getDB = function() {
+		return dbContext;
+	}
+});
+
+testApp.controller('loginController', ['$scope', '$location', 'dbService',
+		function($scope, $location, dbService) {
+
+			var db = dbService.getDB();
+
+			if (db.getAuth()) {
+				$location.path('/chat')
+			}
+
+			var passwordNotSame = function(p1, p2) {
+				$scope.hasError = p1 != p2;
+				return p1 != p2;
+			}
+
+			var login = function(email, password) {
+				db.authWithPassword({
+					"email": email,
+					"password": password
+				}, function(error, authData) {
+					if (error) {
+						alert("Login Failed!", error);
+					} else {
+						$scope.$apply(function() {
+							$location.path('/chat');
+						});
+					}
+				});
+			}
+
+			$scope.signIn = function() {
+				if ($scope.isRegister) {
+					if (passwordNotSame($scope.password, $scope.confirm)) return;
+
+					db.createUser({
+						email: $scope.email,
+						password: $scope.password
+					}, function(error) {
+						if (error) {
+							switch (error.code) {
+								case "EMAIL_TAKEN":
+									alert("The email is already in use.");
+									break;
+								case "INVALID_EMAIL":
+									alert("The specified email is not a valid email.");
+									break;
+								default:
+									console.log("Error creating user:", error);
+							}
+						} else {
+							login($scope.email, $scope.password);
+						}
+					});
+				} else {
+					login($scope.email, $scope.password);
+				}
+			}
+
+		}
+	])
+	.controller('chatController', ['$scope', '$location', 'dbService',
+
+		function($scope, $location, dbService) {
+
+			var db = dbService.getDB();
+
+			if (!db.getAuth()) {
+				$location.path('/')
+			}
+
+			$scope.signOut = function() {
+				db.unauth();
+				$location.path('/')
+			}
+		}
+	])
