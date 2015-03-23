24fc1be7f6d9bdc61ee9865f444062bae1abfafe
JohnnyCage <jaguarsy14@gmail.com>
2015-03-23 17:03:21 +0800

    add chess

---
+++ chess/js/index.js
@@ -0,0 +1,45 @@
+$(function() {
+	var isCompute = false
+
+	var init = function() {
+		var div = $('<div>').addClass('item'),
+			chessPanel = $('#chess-panel'),
+			piecePanel = $('#piece-panel')
+
+		for (var i = 0; i < 196; i++) {
+			chessPanel.append(div.clone());
+		}
+
+		for (var i = 0; i < 225; i++) {
+			piecePanel.append(div.clone());
+		}
+	}
+
+	init()
+
+	var pieces = $('#piece-panel .item');
+
+	var move = function(piece, index) {
+		piece.removeClass('prepare-' + index);
+		piece.addClass('active-' + index);
+		piece.attr('data-value', index);
+	}
+
+	var isWin = function() {}
+
+	var compute = function() {
+		score(pieces);
+	}
+
+	pieces.hover(function() {
+		$(this).addClass('prepare-1');
+	}, function() {
+		$(this).removeClass('prepare-1');
+	})
+
+	pieces.on('click', function() {
+		if (isWin()) return;
+		move($(this), 1);
+		compute();
+	})
+})
