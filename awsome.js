1fdccd4fa24110c7e36a9f79870c0becad952264
商煜 <jaguarsy14@gmail.com>
2015-06-25 16:15:28 +0800

    Update awsome.js

--- awsome.js
+++ awsome.js
@@ -1,7 +1,7 @@
 $.get('/Universal/Do_AddArticleScore.aspx?aid=486460&random=' + new Date().getTime());
 
-$('table th')[0].innerText = '你会发现这个标题跟外面的不一样';
-$('#articlecontent h1')[0].innerText = '你会发现这个标题跟下面的标题也不一样';
+$('table th')[0].innerText = '你会发现这个标题跟下面的标题也不一样';
+$('#articlecontent h1')[0].innerText = '你会发现这个标题跟外面的不一样';
 document.title = '你甚至会发现...';
 
 var loadCss = function(path) {
