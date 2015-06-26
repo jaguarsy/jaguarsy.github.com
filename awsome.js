e1428268bd7139eca0a19c7659230cff89c43a67
JohnnyCage <jaguarsy14@gmail.com>
2015-06-26 10:30:47 +0800

    change awsome.js.

--- awsome.js
+++ awsome.js
@@ -1,19 +1,22 @@
-$.get('/Universal/Do_AddArticleScore.aspx?aid=486460&random=' + new Date().getTime());
+(function() {
+	$.get('/Universal/Do_AddArticleScore.aspx?aid=486460&random=' + new Date().getTime());
 
-$('table th')[0].innerText = '你会发现这个标题跟下面的标题也不一样';
-$('#articlecontent h1')[0].innerText = '你会发现这个标题跟外面的不一样';
-document.title = '你甚至会发现...';
+	$('table th')[0].innerText = '你会发现这个标题跟下面的标题也不一样';
+	$('#articlecontent h1')[0].innerText = '你会发现这个标题跟外面的不一样';
+	document.title = '你甚至会发现...';
 
-var loadCss = function(path) {
-	if (!path || path.length === 0) {
-		throw new Error('argument "path" is required !');
+	var loadCss = function(path) {
+		if (!path || path.length === 0) {
+			throw new Error('argument "path" is required !');
+		}
+		var head = document.getElementsByTagName('head')[0],
+			link = document.createElement('link');
+			
+		link.href = path;
+		link.rel = 'stylesheet';
+		link.type = 'text/css';
+		head.appendChild(link);
 	}
-	var head = document.getElementsByTagName('head')[0];
-	var link = document.createElement('link');
-	link.href = path;
-	link.rel = 'stylesheet';
-	link.type = 'text/css';
-	head.appendChild(link);
-}
 
-loadCss('http://jaguarsy.github.io/awsome.css');
+	loadCss('http://jaguarsy.github.io/awsome.css');
+})()
