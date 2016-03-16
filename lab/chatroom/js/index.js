5e5a3bf8dfa63e95183c2e53a70175369ebbc260
JohnnyCage <shangyu@cheyaoshi.com>
2016-03-16 19:27:16 +0800

    统一时间格式

--- lab/chatroom/js/index.js
+++ lab/chatroom/js/index.js
@@ -3,6 +3,28 @@
  */
 (function ($, undefined) {
 
+    Date.prototype.format = function (fmt) {
+        var o = {
+            "M+": this.getMonth() + 1,
+            "[Dd]+": this.getDate(),
+            "[Hh]+": this.getHours(),
+            "m+": this.getMinutes(),
+            "s+": this.getSeconds(),
+            "q+": Math.floor((this.getMonth() + 3) / 3),
+            "S": this.getMilliseconds()
+        };
+        if (/([Yy]+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o) {
+            if (!o.hasOwnProperty(k)) {
+                continue;
+            }
+            if (new RegExp("(" + k + ")").test(fmt)) {
+                fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+            }
+        }
+        return fmt;
+    };
+
     $(function () {
 
         var db = new Wilddog("https://wild-horse-6428.wilddogio.com/chat"),
@@ -29,6 +51,8 @@
             }
 
             target.append(message);
+
+            target[0].scrollTop = target[0].scrollHeight
         };
 
         var refreshChat = function (target) {
@@ -90,7 +114,7 @@
 
             var name = $name.val(),
                 content = $content.val(),
-                time = new Date().toLocaleString();
+                time = new Date().format('yyyy-MM-dd');
 
             if (!name) {
                 message.error('请先输入姓名');
